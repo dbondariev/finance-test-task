f9cac40e4c3061bf3d63da669408828c4e73fdf8
Dima Bondariev <dima.bondariev@gmail.com>
2023-10-29 16:28:14 +0200

    got rid of interval and made sure data auto updates and re-renders

--- client/src/components/TickerList.js
+++ client/src/components/TickerList.js
@@ -2,58 +2,26 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import Ticker from "./Ticker";
 
-const socket = io("http://localhost:4000"); // Set the correct URL
+const socket = io("http://localhost:4000"); 
 
 const TickerList = () => {
   const [tickers, setTickers] = useState([]);
 
   useEffect(() => {
-    // Function to fetch new data from the server
     const fetchData = () => {
       socket.emit("start");
     };
 
-    // Initial data fetch
     fetchData();
 
-    // Set up a timer to fetch new data every 5 seconds
-    const timer = setInterval(fetchData, 5000);
+    socket.on("ticker", (data) => {
+      setTickers(data); 
+    });
 
-    // Clean up the timer when the component unmounts
     return () => {
-      clearInterval(timer);
+      socket.off("ticker");
     };
   }, []);
-
-  useEffect(() => {
-    socket.on("ticker", (data) => {
-      // Handle incoming ticker data
-      // Flatten the received array of arrays
-      const flattenedData = data.flat();
-      setTickers((prevTickers) => {
-        // Merge the new data with the existing data, preserving unique tickers
-        const mergedTickers = [...prevTickers, ...flattenedData].reduce(
-          (acc, ticker) => {
-            let existingTicker = acc.find((t) => t.ticker === ticker.ticker);
-            if (existingTicker) {
-              // Update the existing ticker
-              existingTicker = { ...existingTicker, ...ticker };
-            } else {
-              // Add a new ticker
-              acc.push(ticker);
-            }
-            return acc;
-          },
-          [],
-        );
-        return mergedTickers;
-      });
-    });
-  }, []);
-
-  useEffect(() => {
-    console.log("Updated Tickers", tickers);
-  }, [tickers]);
 
   return (
     <div className="ticker-list">
